ff38e95c15e4552a008c6baa2b9518bf7e1d29d7
Tung Thanh <97510320030@ispace.edu.vn>
2024-01-28 15:02:10 +0700

    feat: add about and skill page

---
+++ src/Main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import About from "./About";
+import Skills from "./Skills";
+
+export default function Main() {
+  return (
+    <div
+      className="grow bg-cover pt-16 bg-center relative"
+      style={{
+        backgroundImage: "url('/dev.jpeg')",
+      }}
+    >
+      <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center backdrop-blur-md">
+        <div className="w-full lg:w-3/5 text-white dark:text-orange ">
+          <h1 className="text-4xl lg:text-6xl font-semibold px-4 text-center">
+            Hello there. Welcome to my CV.
+          </h1>
+          <h2 className="text-2xl lg:text-3xl font-semibold px-4 text-center mt-16">
+            I am Vu Nguyen Thanh Tung, a student at iSpace. I am currently a new
+            developer.
+            <br />
+            <br />I am living in Ho Chi Minh city and seeking a job near me.
+          </h2>
+        </div>
+      </div>
+      <div className="w-full h-full" id="home"></div>
+      <About></About>
+      <Skills></Skills>
+    </div>
+  );
+}
---
+++ src/Skills.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div
+      id="skills"
+      className="w-full h-full flex flex-col items-center justify-center px-4 lg:px-10 py-10 dark:bg-white dark:text-black 
+          bg-black text-white gap-y-10"
+    >
+      <div className="flex flex-col items-center gap-y-10 max-w-[800px]">
+        <h2 className="text-4xl lg:text-6xl font-bold">My skills</h2>
+        <img
+          src="/skills.png"
+          alt="avatar"
+          className="w-32 h-32 rounded-full object-cover"
+        />
+        <h3 className="text-2xl lg:text-3xl text-center">
+          <ul>
+            <li>HTML, CSS, JS</li>
+            <li>.NET</li>
+            <li>React</li>
+            <li>Problem solving</li>
+            <li>Teamwork</li>
+          </ul>
+        </h3>
+      </div>
+    </div>
+  );
+}
